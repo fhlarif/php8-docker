a8dfc083bd894faed59ceb33b9cba73559451d87
fhlarif <fathulkb@gmial.com>
2022-09-01 00:34:11 +0800

    add tailwind to index

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Training</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
-echo '<pre>';
+<body class="bg-gray-900 text-gray-200">
+    <main class="flex justify-center items-center h-screen text-3xl">
+        <?php echo "hello There!" ?>
+    </main>
+</body>
 
-print_r($_SERVER);
-
-echo '</pre>';
+</html>
